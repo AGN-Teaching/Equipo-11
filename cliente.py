51039bfa98befb35c1a3ba7e2b81345fbfec85df
AnaidOR <ana_id03@outlook.com>
2023-10-13 13:26:03 -0600

    correciones

--- cliente.py
+++ cliente.py
@@ -3,8 +3,6 @@ class Cliente:
         self.nombre = nombre
         self.identificacion = identificacion
         self.tarjeta_credito = tarjeta_credito
-        #self.fecha_caducidad =fecha_caducidad
-        #self.cvv = cvv
 
     def guardar_datos(self):
         return self.nombre+","+self.identificacion+","+self.tarjeta_credito
